ce7436fb0e1ad46f65330aafe03f298304380003
Megan Daly <mmdaly@linpe238vm7.csbsju.edu>
2016-02-23 15:15:29 -0600

    Terminal 2 in Step 5: Modified Employee.java introducing a conflict

--- edu/csbsju/cs/Employee.java
+++ edu/csbsju/cs/Employee.java
@@ -30,7 +30,7 @@ public class Employee
       return hireDay;
    }
 
-   public void raiseSalary(double byPercent)
+   public void raiseSalary(double byDecimalValue)
    {
       double raise = salary * byPercent / 100;
       salary += raise;
